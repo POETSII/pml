01a0b3fd485a49495da7b0d7c185546aa276ae50
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-04-18 19:19:06 +0100

    Fix issue in 'simple' send_handler

--- templates/simple/send_handler.c
+++ templates/simple/send_handler.c
@@ -2,7 +2,8 @@ if (deviceState->{{ msg }}_buffer_ptr == 0) {
 	// If this is executed, it is most likely due to an error in ready_to_send
 
 	// TODO: find out why calling handler_exit here causes an error
-	// handler_exit("Error, attempted to send while buffer is empty");
+	handler_log(2, "Error, attempted to send while buffer is empty");
+	handler_exit(1);
 }
 
 uint32_t ind = --(deviceState->{{ msg }}_buffer_ptr);
