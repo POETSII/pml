7e43c8660a6ffff07a4c881b66604de7e54aa3e8
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-04-18 19:27:12 +0100

    Prettify simple/send_handler.c

--- templates/simple/send_handler.c
+++ templates/simple/send_handler.c
@@ -1,7 +1,5 @@
 if (deviceState->{{ msg }}_buffer_ptr == 0) {
 	// If this is executed, it is most likely due to an error in ready_to_send
-
-	// TODO: find out why calling handler_exit here causes an error
 	handler_log(2, "Error, attempted to send while buffer is empty");
 	handler_exit(1);
 }
@@ -11,9 +9,6 @@ uint32_t ind = --(deviceState->{{ msg }}_buffer_ptr);
 message->src = deviceProperties->id;
 message->dst = deviceState->{{ msg }}_buffer_dst[ind];
 
-
-// {{content}}
-
-{% for fd_id in fields.keys() %}
+{%- for fd_id in fields.keys() %}
 message->{{ fd_id }} = deviceState->{{ msg }}_buffer_{{ fd_id }}[ind];
-{% endfor %}
+{%- endfor %}
