b5721bf9631f1c1b82f64ce4256d8460f0ddeaa5
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-08-16 15:36:43 +0100

    Add support for loading property files

--- pml.py
+++ pml.py
@@ -11,30 +11,66 @@ from generator import get_path
 usage = """pml v0.1
 
 Usage:
-  pml.py [-p <name:value>...] <app.json> <file.graphml>
+  pml.py [options] <app.json> <file.graphml>
 
 Options:
-  -p, --param <name:value>  Specify a code generation parameter.
+  -p, --param <name:value>  Specify code generation parameter(s).
+  -o, --props <file.json>   Load property values from file.
 
 """
 
 
-def build(app_file, graphml_file, params=dict()):
+def read_json(file):
+    """Read a JSON file."""
+    with open(file, "r") as fid:
+        return json.load(fid)
+
+
+def get_prop_strings(props_file):
+    """Parse a propery file.
+
+    Property files are JSON files containing a mapping node -> properties.
+
+    This function returns a corresponding mapping node -> property strings
+    where a "property string" is a string serializing of the properties
+    object, excluding the leading and training brackets.
+
+    For example, if props_file contains:
+
+    {
+        "node1": {"w": 10, "h": 10},
+        "node2": {"w": 20, "h": 20},
+    }
+
+    then this functions returns:
+
+    {
+        "node1": '"w": 10, "h": 10',
+        "node2": '"w": 20, "h": 20',
+    }
+
+    """
+
+    if not props_file:
+        return {}
+
+    return {node: json.dumps(props)[1:-1]
+            for node, props in read_json(props_file).iteritems()}
+
+
+def build(app_file, graphml_file, props_file, params=dict()):
     """Generate an XML file from app and graph files.
 
     Args:
         app_file (str): Path to application JSON file.
         graphml_file (str): Path to GraphML file.
+        props_file (str): Path to properties file (optional).
+        params (dict): Generation parameters (optional).
 
     Returns:
-        str: xml file content.
+        str: XML file content.
 
     """
-
-    def read_json(file):
-        """Read a JSON file."""
-        with open(file, "r") as fid:
-            return json.load(fid)
 
     content = read_json(app_file)
 
@@ -72,6 +108,7 @@ def build(app_file, graphml_file, params=dict()):
         raise Exception("Could not determine length of field %s" % field)
 
     content['params'] = params
+    content['props'] = get_prop_strings(props_file)
     graph = Graph(graphml_file)
     template = 'templates/%s/template.xml' % content["model"]
     env_globals = {
@@ -81,18 +118,21 @@ def build(app_file, graphml_file, params=dict()):
     xml = generate_xml(template, graph, env_globals, content)
     return xml
 
-def parse_params(param_list):
-    """Convert a parameter list into a dictionary.
 
-    Each item in the list is a string in the form 'name:value'.
+def parse_params(param_str):
+    """Convert a parameter string into a dictionary.
+
+    The string is in the format "name1:val1,name2:val2 ...".
 
     Args:
-      param_list (list): list of parameter strings
+      param_str (string): parameter string
 
     Returns:
       dict: dictionary of parameters
 
     """
+
+    param_list = [item for item in param_str.split(",") if item]
 
     def split_param(param_str):
         parts = param_str.split(':')
@@ -105,9 +145,10 @@ def parse_params(param_list):
 def main():
     args = docopt(usage, version="pml v0.1")
     app_file = args["<app.json>"]
+    props_file = args["--props"]
     graphml_file = args["<file.graphml>"]
-    params = parse_params(args["--param"])
-    xml = build(app_file, graphml_file, params)
+    params = parse_params(args["--param"] or "")
+    xml = build(app_file, graphml_file, props_file, params)
     print xml
 
 
