13c64c2cac707d69d182338033e0306b27b97338
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-08-20 20:09:06 +0100

    Count descendant nodes

--- networks/main.c
+++ networks/main.c
@@ -20,12 +20,21 @@ void start(STATE_PROP_ARGS) {
 
 void visit(STATE_PROP_ARGS, req_msg* outgoing) {
     handler_log(1, "Visit (visitor_id = %d)", outgoing->visitor_id);
+
+    if (outgoing->visitor_id == 5)
+        deviceState->ndescendants = 0;
 }
 
 void map(STATE_PROP_ARGS, ack_msg* outgoing){
     handler_log(1, "Map (visitor_id = %d)", outgoing->visitor_id);
+
+    if (outgoing->visitor_id == 5)
+        outgoing->payload = deviceState->ndescendants + 1;
 }
 
 void reduce(STATE_PROP_ARGS, const network_ack_message_t* message) {
     handler_log(1, "Reduce msg from %d (visitor_id = %d)", message->src, message->visitor_id);
+
+    if (message->visitor_id == 5)
+        deviceState->ndescendants += message->payload;
 }
